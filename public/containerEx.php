a1d92b2132bcde5696ab59a8b4792d87e6390de4
JP <jpc.araujoti@gmail.com>
2017-02-22 00:40:12 -0300

    Instanciando objetos com Pimple

--- public/containerEx.php
+++ public/containerEx.php
@@ -6,8 +6,19 @@ use Pimple\Container;
 
 $container = new Container();
 
+//Singleton do objeto
 $container['date'] = function (){
     return new \datetime;
 };
+
+//Fabrica sempre um novo objeto
+$container['date'] = $container->factory(function (){
+    return new \datetime;
+});
+
+var_dump($container['date']);
+echo "<br/>";
+echo "<br/>";
+sleep(2);
 
 var_dump($container['date']);
